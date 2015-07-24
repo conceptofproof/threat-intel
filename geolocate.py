20bf300432dceb440033edde021c7b0c1e9f6296
conceptofproof <joshua.wang.90@gmail.com>
2015-07-23 20:57:40 -0400

    1st draft

---
+++ geolocate.py
@@ -0,0 +1,27 @@
+#/usr/bin/python
+import pygeoip
+import os,sys
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+          return os.path.join(root, name)
+
+def locateip(ip):
+ return  record_by_addr
+
+try:
+  gi = pygeoip.GeoIP('/home/rh0gue/Downloads/GeoLiteCity.dat')
+except:
+  gi = pygeoip.GeoIP(find('GeoLiteCity.dat','//'))
+
+try:
+  for i in range(1,len(sys.argv)):
+    ip = sys.argv[i]
+    print ip
+    result = gi.record_by_addr(ip)
+    for key,val in result.items():
+      print "[*] "+str(key)+" : "+str(val)
+    print "\n"
+except:
+  print "Usage: geolocate <ip address(s)>"
